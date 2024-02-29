6d486f18c323b83eae6ec4fc12e7961a153c1a43
kusurin <kusu_rin@sjtu.edu.cn>
2024-02-29 22:50:01 +0800

    fix: last fix

--- auto.js
+++ auto.js
@@ -91,7 +91,7 @@ async function main() {
                 retry_times = 0;
                 retry_times_limit = 6;
                 while (play_buttons[0] == null || progress_holder[k].getAttribute("aria-valuenow") != progress || progress < 100.0 || isNaN(progress)) {
-                    if (progress_holder[k].getAttribute("aria-valuenow") == progress && progress < 100.0) {
+                    if (play_buttons[0] == null || progress_holder[k].getAttribute("aria-valuenow") == progress && progress < 100.0) {
                         console.log("video stopped and unfinished.retrying " + (retry_times + 1) + "/" + retry_times_limit);
                         chaps.children[0].children[2].children[0].click();
                         await sleep(rand_range(1000, 2000));
