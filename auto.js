058631d99bea674af8d87f6232b885a04d53cdf8
kusurin <kusu_rin@sjtu.edu.cn>
2024-03-01 09:29:37 +0800

    fix: couldn't recognize play err

--- auto.js
+++ auto.js
@@ -91,7 +91,7 @@ async function main() {
                 retry_times = 0;
                 retry_times_limit = 6;
                 while (play_buttons[0] == null || progress_holder[k].getAttribute("aria-valuenow") != progress || progress < 100.0 || isNaN(progress)) {
-                    if (play_buttons[0] == null || (progress_holder[k].getAttribute("aria-valuenow") == progress && progress < 100.0 && window.getComputedStyle(document.getElementsByClassName("practice-box")[0]).display == "none")) {
+                    if (document.getElementsByClassName("video-error")[0] != null || play_buttons[0] == null || (progress_holder[k].getAttribute("aria-valuenow") == progress && progress < 100.0 && window.getComputedStyle(document.getElementsByClassName("practice-box")[0]).display == "none")) {
                         console.log("video stopped and unfinished.retrying " + (retry_times + 1) + "/" + retry_times_limit);
                         chaps.children[0].children[2].children[0].click();
                         await sleep(rand_range(1000, 2000));
